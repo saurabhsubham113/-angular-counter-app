cae5fe4ab623af65dedac67a0a0650fa6b99edc2
subham saurabh <saurabhsubahm113@gmail.com>
2021-09-14 15:52:37 +0530

    counter completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,22 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'counter-app';
+  title: string = 'Counter App';
+
+  count: number = 0;
+
+  handleIncrement = () => {
+    console.log(this.count);
+    
+    this.count < 10 ? (this.count += 1 ): 10;
+  };
+  handleDecrement = () => {
+    this.count > 0 ? (this.count -= 1) : 0;
+  };
+  handleReset = () => {
+    this.count = 0;
+  };
 }
